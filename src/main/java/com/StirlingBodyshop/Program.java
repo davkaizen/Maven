5715525e57617a1a37e9c5615947773d96fd0de2
DR <DR@kaizen.com>
2024-11-26 15:14:19 +0000

    Updated Program.java file

--- src/main/java/com/StirlingBodyshop/Program.java
+++ src/main/java/com/StirlingBodyshop/Program.java
@@ -2,4 +2,4 @@ package com.StirlingBodyshop;
 
 public class Program { public static void main(String[] args) 
 
-{ System.out.println("This program is a Maven project and the package is com.J1ggy"); } } 
+{ System.out.println("This program is a Maven project and the package is com.StirlingBodyshop"); } } 
